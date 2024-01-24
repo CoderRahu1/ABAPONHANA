0864f3db7767a180001f82dbb06252d3619e7d79
Er.Rahul Pillai <94741905+CoderRahu1@users.noreply.github.com>
2024-01-24 16:48:49 +0530

    Update ZRAHUL_HANA_13.sql

--- ZRAHUL_HANA_13.sql
+++ ZRAHUL_HANA_13.sql
@@ -21,4 +21,23 @@ END;
 --------------------------------------------------------------
 -- CALL CALCFACT(?,?)
 
+--------------------------------------------------------------
+-- OTHER SET OF CODE.
+
+ALTER PROCEDURE CALCFACT(IN X INTEGER, OUT Y DECIMAL(20,4))
+AS BEGIN
+
+Y = 1;
+
+WHILE :X > 0 DO 
+	Y := :Y * :X ;
+	X := :X - 1;
+	
+END WHILE;	
+	
+	
+
+END;
+
+
 		
