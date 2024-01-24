819933173d60a46cb66c63aed41c4b3099eca261
Er.Rahul Pillai <94741905+CoderRahu1@users.noreply.github.com>
2024-01-24 16:41:05 +0530

    Create ZRAHUL_HANA_13.sql

---
+++ ZRAHUL_HANA_13.sql
@@ -0,0 +1,22 @@
+-- TOPIC - 1) USING WHILE LOOP INSIDE STORED PROCEDURE.
+
+--------------------------------------------------------------
+-- REQ - CREATE A STORED PROCEDURE FOR CALCULATING THE FACTORIAL OF A NUMBER.
+
+
+
+CREATE PROCEDURE CALCFACT(IN X INTEGER, OUT Y DECIMAL(20,4))
+AS BEGIN
+
+Y = 1;
+
+WHILE :X > 0 DO 
+	Y := :Y * ( :X * 10);
+	X := :X - 1;
+	
+END WHILE;	
+	
+	
+
+END;
+		
